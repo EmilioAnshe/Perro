bbb854d95084804234622ea080389e307ecb811c
Emilio <100852610+EmilioAnshe@users.noreply.github.com>
2024-06-11 21:57:15 -0300

    Add files via upload

---
+++ Perroo/Objeto/AtriPerro.cs
@@ -0,0 +1,23 @@
+﻿using Objeto1;
+
+namespace libreria{
+    
+    public class Perro{
+
+        public string raza;
+        public int edad;
+        public string tamano;
+        public void Jugar(string pJugar)
+        {
+            Console.WriteLine("Jugando....." + pJugar);
+        }
+        public string ladrar()
+        {
+            return "guau guau";
+        }
+    }
+}
+
+
+
+
---
+++ Perroo/Objeto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using libreria;
+
+namespace Objeto1{
+
+    public class Principal{
+
+        public static void Main()
+        {
+            string nombre;
+
+
+            nombre = "Juan";
+            Perro Firulais= new Perro();
+            Perro pedro = new Perro();
+
+
+            Console.WriteLine("hola");
+
+            Firulais.edad = 2;
+
+            Firulais.tamano = "Grande";
+
+            Firulais.raza = "caniche";
+
+            Firulais.ladrar();
+
+            Firulais.Jugar("Saltar");
+            Firulais.Jugar("dormirse");
+
+        }
+    }
+}
